85f3392a926f484c2f4d424b65fd5174c25ce222
Hieu <vuminhhieua12910@gmail.com>
2021-12-01 21:41:39 +0700

    update guest

--- server-1/src/main/java/demo/service/GuestServiceImpl.java
+++ server-1/src/main/java/demo/service/GuestServiceImpl.java
@@ -1,6 +1,7 @@
 package demo.service;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import demo.dto.GuestDTO;
+import demo.dto.StudentDTO;
 import demo.entity.GuestEntity;
 import demo.entity.ResponseObject;
 import demo.entity.StudentEntity;
@@ -34,10 +36,20 @@ public class GuestServiceImpl implements GuestService {
 
 		GuestEntity guestEntity = new GuestEntity();
 		guestEntity = modelMapper.map(guest, GuestEntity.class);
+	//	StudentEntity studentEntity = studentRepository.findById(guest.getStudentID());
+	//	System.out.println(studentEntity.getId());
 		StudentEntity studentEntity = studentRepository.findById(guest.getStudentID());
+	//	System.out.println(studentRepository.findById(studentEntity.getId()));
+	//	studentRepository.findAll(studentEntity.getId()).forEach()
 		guestEntity.setStudentID(studentEntity);
-		guestEntity = guestRepository.save(guestEntity);
-		return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("ok", "create successfully", ""));
+		if(studentEntity.getId() == guest.getStudentID() ) {
+			guestEntity = guestRepository.save(guestEntity);
+			return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("ok", "create successfully", ""));
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
+					.body(new ResponseObject("failed", "Insert Guest successfully", ""));
+		}
+		
 
 
 	}
@@ -63,9 +75,18 @@ public class GuestServiceImpl implements GuestService {
 	public List<GuestDTO> getAll() {
 		List<GuestDTO> results = new ArrayList<>();
 		List<GuestEntity> entities = guestRepository.findAll();
+		List<StudentEntity> studentEntities = studentRepository.findAll();
 		for (GuestEntity item : entities) {
-			GuestDTO guestDTO = modelMapper.map(item, GuestDTO.class);
-			results.add(guestDTO);
+			for(StudentEntity student : studentEntities) {
+				GuestDTO guestDTO = modelMapper.map(item, GuestDTO.class);
+				if(student.getId() == guestDTO.getStudentID() ) {
+					StudentDTO studentDTO = modelMapper.map(student, StudentDTO.class);
+					guestDTO.setStudentObject(studentDTO);
+					results.add(guestDTO);
+				}
+					
+			}
+			
 		}
 		return results;
 
