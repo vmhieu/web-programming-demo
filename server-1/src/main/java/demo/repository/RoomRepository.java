b25eecef3bcbf239244dfc2d4c18450026f69be5
duck <trinhduc2712@gmail.com>
2021-12-09 21:11:10 +0700

    Plus room free in bill

--- server-1/src/main/java/demo/repository/RoomRepository.java
+++ server-1/src/main/java/demo/repository/RoomRepository.java
@@ -1,9 +1,10 @@
 package demo.repository;
 
+import demo.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import demo.entity.RoomEntity;
 
 public interface RoomRepository extends JpaRepository<RoomEntity, Long>{
-	
+	RoomEntity findByStudentRoom(StudentEntity studentEntity);
 }
